52a89ebd798bd25f83da88e35c16c5a5642f166c
IvanMaximov <m2205156@edi.misis.ru>
2024-07-08 17:20:24 +0300

    added comments to tests ans main

--- practice2024/app.py
+++ practice2024/app.py
@@ -2,6 +2,9 @@ from .optimized import optimized
 
 
 # функция main
+# на вход принимает json-строку с исходными данными
+# возвращает таблицу одной колонкой - искомый набор имен признаков
+# в виде csv-строки
 def main(input):
     output = optimized(input)
     return output
--- practice2024/tests.py
+++ practice2024/tests.py
@@ -7,24 +7,28 @@ from .optimized import optimized
 from .app import main
 
 
+# тесты для функции load_json файла modules.py
 class LoadJsonTestCase(TestCase):
     def test_load_json(self):
         json_string = '[{"a": 0, "b": 1}, {"b": 2, "c": 3}]'
         self.assertEqual(load_json(json_string), [{'a': 0, 'b': 1}, {'b': 2, 'c': 3}])
 
 
+# тесты для функции get_properties файла modules.py
 class GetPropertiesTestCase(TestCase):
     def test_get_properties(self):
         objects = [{'a': 0, 'b': 1}, {'b': 2, 'c': 3}]
         self.assertEqual(get_properties(objects), (['a', 'b', 'c'], 3))
 
 
+# тесты для функции get_subsets файла modules.py
 class GetSubsetsTestCase(TestCase):
     def test_get_subsets(self):
         properties = ['a', 'b', 'c']
         self.assertEqual(get_subsets(properties), [['a', 'b', 'c'], ['a', 'b'], ['a', 'c'], ['b', 'c'], ['a'], ['b'], ['c']])
 
 
+# тесты для функции group_by_len файла modules.py
 class GroupByLenTestCase(TestCase):
     def test_group_by_len(self):
         subsets = [['a', 'b', 'c'], ['a', 'b'], ['a', 'c'], ['b', 'c'], ['a'], ['b'], ['c']]
@@ -32,30 +36,35 @@ class GroupByLenTestCase(TestCase):
         self.assertEqual(group_by_len(subsets, m), [[], [['a'], ['b'], ['c']], [['a', 'b'], ['a', 'c'], ['b', 'c']], [['a', 'b', 'c']]])
 
 
+# тесты для функции list_to_df файла modules.py
 class ListToDfTestCase(TestCase):
     def test_list_to_df(self):
         lst = ['a', 'c']
         self.assertEqual(list_to_df(lst).equals(pd.DataFrame(['a', 'c'])), True)
 
 
+# тесты для функции write_csv файла modules.py
 class WriteCsvTestCase(TestCase):
     def test_write_csv(self):
         df = pd.DataFrame(['a', 'c'])
         self.assertEqual(write_csv(df), 'a\r\nc\r\n')
 
 
+# тесты для функции brute файла brute.py
 class BruteTestCase(TestCase):
     def test_brute(self):
         json_string = '[{"a": 0, "b": 1, "c": 0}, {"a": 0, "b": 2, "c": 3}, {"b": 3, "c": 3}]'
         self.assertEqual(brute(json_string), 'b\r\n')
 
 
+# тесты для функции optimized файла optimized.py
 class OptimizedTestCase(TestCase):
     def test_optimized(self):
         json_string = '[{"a": 0, "b": 1, "c": 0}, {"a": 0, "b": 2, "c": 3}, {"b": 3, "c": 3}]'
         self.assertEqual(optimized(json_string), 'b\r\n')
 
 
+# тесты для функции main файла app.py
 class MainTestCase(TestCase):
     def test_main(self):
         json_string = '[{"a": 0, "b": 1, "c": 0}, {"a": 0, "b": 2, "c": 3}, {"b": 3, "c": 3}]'
